0aeda27d5fa5d1d90227090c81faf2d6d826f751
Khoa Le <khoa.le@hrboss.com>
2016-10-20 17:12:42 +0700

    pep8

--- s3-signed-url.py
+++ s3-signed-url.py
@@ -19,14 +19,16 @@ USE CASES:
 
 USAGE:
     ./s3-signed-url.py -p {PATH} -t {EXPIRY_TIME_IN_SECOND}
-    ./s3-signed-url.py -p {PATH} 
+    ./s3-signed-url.py -p {PATH}
 
 DESCRIPTION:
     -p    --path            The path of the s3 object (include S3 Bucket)
     -t    --time            Expiry time in second. Default: 1 hour
 
 EXAMPLES:
-    If the link of your s3 object is https://s3-ap-southeast-1.amazonaws.com/devops/share/share.zip
+    If the link of your s3 object is:
+    https://s3-ap-southeast-1.amazonaws.com/devops/share/share.zip
+    And we want to share in 1 min:
     ------------------------------------------------------------
     ./s3-signed-url.py -p devops/share/share.zip -t 60
     ------------------------------------------------------------
@@ -37,6 +39,7 @@ NOTE:
     export AWS_SECRET_ACCESS_KEY=xxx
 
 """
+
 
 def getenv(env_var):
     """Get environment variable
@@ -69,41 +72,44 @@ def get_s3_signed_url(path, expire):
     bucket = path.split('/')[0]
     obj = '/'.join(path.split('/')[1:])
     print('Bucket:', bucket)
-    print('Object:', obj) 
+    print('Object:', obj)
 
     # Expiry Timestamp
     expiry_ts = int(time.time()) + expire
     h = hmac.new(aws_secret_access_key,
-            "GET\n\n\n%d\n/%s/%s" % (expiry_ts, bucket, obj),
-            sha)
+                 "GET\n\n\n%d\n/%s/%s" % (expiry_ts, bucket, obj),
+                 sha)
 
     # Signature
     sig = urllib.quote_plus(base64.encodestring(h.digest()).strip())
 
-    signed_url = 'http://%s.s3.amazonaws.com/%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (bucket, obj, aws_access_key_id, expiry_ts, sig)
-    
+    signed_url = 'http://%s.s3.amazonaws.com/%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' \
+                 % (bucket, obj, aws_access_key_id, expiry_ts, sig)
+
     return signed_url
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', '-p', help='The path of the s3 object (include S3 Bucket)')
-    parser.add_argument('--time', '-t', help='Expiry time in second. Default: 1 hour')
+    parser.add_argument('--path', '-p',
+                        help='The path of the s3 object (include S3 Bucket)')
+    parser.add_argument('--time', '-t',
+                        help='Expiry time in second. Default: 1 hour')
     args = parser.parse_args()
-   
+
     if args.path is None:
         print(HELP)
         exit(1)
     else:
         path = args.path.strip()
-    
+
     if path[0] == '/':
         path = path[1:]
-    
+
     if args.time is None:
-        expire = 60 * 60 # 1 hour
- 
-    signed_url = get_s3_signed_url(path, expire) 
+        expire = 60 * 60  # 1 hour
+
+    signed_url = get_s3_signed_url(path, expire)
 
     print('Signed url: -------------------------->')
     print(signed_url)
